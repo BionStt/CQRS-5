d32dd99fe1e3f55d482de66d1664a303d4ce7bec
Jonathan Williams <37890156+jonathanjameswilliams26@users.noreply.github.com>
2020-10-30 15:06:16 +1100

    Add files via upload

---
+++ DependencyInjection.cs
@@ -0,0 +1,48 @@
+﻿using CQRS.Behaviours;
+using CQRS.Interfaces;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CQRS.DependencyInjection
+{
+    public static class DependencyInjection
+    { 
+        public static void AddDefaultBehaviours(this IServiceCollection services)
+        {
+            // Add the MediatR behaviour pipeline. Will execute in order of registration.
+            // See: https://github.com/jbogard/MediatR/wiki/Behaviors
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorisationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        }
+
+        public static void AddValidators(this IServiceCollection services, Assembly assembly)
+        {
+            var authorizerType = typeof(IValidator<>);
+            assembly.GetTypesAssignableTo(authorizerType).ForEach((type) =>
+            {
+                foreach (var implementedInterface in type.ImplementedInterfaces)
+                {
+                    services.AddTransient(implementedInterface, type);
+                }
+            });
+        }
+
+        private static List<TypeInfo> GetTypesAssignableTo(this Assembly assembly, Type compareType)
+        {
+            var typeInfoList = assembly.DefinedTypes.Where(x => x.IsClass
+                                && !x.IsAbstract
+                                && x != compareType
+                                && x.GetInterfaces()
+                                        .Any(i => i.IsGenericType
+                                                && i.GetGenericTypeDefinition() == compareType))?.ToList();
+
+            return typeInfoList;
+        }
+    }
+}
---
+++ Extensions.cs
@@ -0,0 +1,20 @@
+﻿using CQRS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS.Extensions
+{
+    public static class Extensions
+    {
+        public static IActionResult ToResponse(this CQRSResponse response)
+        {
+            if (response.IsUnauthorised)
+                return new StatusCodeResult(response.StatusCode);
+            if (response.IsUnsuccessful)
+                return new ObjectResult(new { response.ErrorMessage }) { StatusCode = response.StatusCode };
+            else if (response.HasData)
+                return new ObjectResult(new { Data = response.GetData() }) { StatusCode = response.StatusCode };
+            else
+                return new StatusCodeResult(response.StatusCode);
+        }
+    }
+}
